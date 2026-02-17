d9ce350c818541050ce18062321a81844186799f
AnujShahu <anuj.shahu24@sakec.ac.in>
2026-02-17 15:36:00 +0530

    Update DB_PATH to use dynamic base directory

--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,10 @@ import pandas as pd
 
 main = Blueprint("main", __name__)
 
-DB_PATH = "anime.db"
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DB_PATH = os.path.join(BASE_DIR, "anime.db")
 
 
 # ================= HOME =================
