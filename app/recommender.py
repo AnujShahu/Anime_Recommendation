51eabff24a045bc52fdca953382947b1410fe5cf
AnujShahu <anuj.shahu24@sakec.ac.in>
2026-02-17 15:35:43 +0530

    Refactor DB_PATH to use BASE_DIR
    Update database path to be relative to the base directory.

--- app/recommender.py
+++ app/recommender.py
@@ -1,7 +1,11 @@
 import sqlite3
 import pandas as pd
 
-DB_PATH = "anime.db"
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DB_PATH = os.path.join(BASE_DIR, "anime.db")
+
 
 def get_connection():
     return sqlite3.connect(DB_PATH)
