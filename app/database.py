0b747babe38a4ce97bdf8a0fdf37c9d3f6c63fdd
Anuj Shahu <your_github_email_hereanuj.shahu24@sakec.ac.in>
2026-02-27 12:56:58 +0530

    database route change

--- app/database.py
+++ app/database.py
@@ -1,8 +1,11 @@
 import sqlite3
 import os
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-db_path = os.path.join(BASE_DIR, "anime.db")
 
 def get_connection():
-    return sqlite3.connect(db_path)
+    base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    db_path = os.path.join(base_dir, "anime.db")
+
+    conn = sqlite3.connect(db_path)
+    conn.row_factory = sqlite3.Row
+    return conn
